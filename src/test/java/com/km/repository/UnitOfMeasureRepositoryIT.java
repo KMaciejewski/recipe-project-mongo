166d3a41f037c84af5f7a3fcc7d4fd35d602db47
kmaciejewski <kamil.maciejewski@hotmail.com>
2020-11-16 14:43:57 +0100

    UOM integration test

--- src/test/java/com/km/repository/UnitOfMeasureRepositoryIT.java
+++ src/test/java/com/km/repository/UnitOfMeasureRepositoryIT.java
@@ -1,28 +1,46 @@
-//package com.km.unit.repository;
-//
-//import com.km.model.UnitOfMeasure;
-//import com.km.repository.UnitOfMeasureRepository;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//
-//import java.util.Optional;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//@ExtendWith(SpringExtension.class)
-//@DataJpaTest
-//class UnitOfMeasureRepositoryIT {
-//
-//    @Autowired
-//    private UnitOfMeasureRepository unitOfMeasureRepository;
-//
-//    @Test
-//    void findByDescription() {
-//        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
-//
-//        assertEquals("Teaspoon", uomOptional.get().getDescription());
-//    }
-//}
+package com.km.repository;
+
+import com.km.bootstrap.RecipeBootstrap;
+import com.km.model.UnitOfMeasure;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@DataMongoTest
+class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private RecipeRepository recipeRepository;
+
+    @Autowired
+    private IngredientRepository ingredientRepository;
+
+    private RecipeBootstrap recipeBootstrap;
+
+    @BeforeEach
+    void setUp() {
+        recipeBootstrap = new RecipeBootstrap(categoryRepository, recipeRepository, unitOfMeasureRepository, ingredientRepository);
+        recipeBootstrap.onApplicationEvent(null);
+    }
+
+    @Test
+    void findByDescription() {
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        assertEquals("Teaspoon", uomOptional.get().getDescription());
+    }
+}
